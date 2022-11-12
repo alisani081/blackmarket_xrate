2be145f7ffa8f60852109e81d392b82ebe7952df
alisani081 <960395b7a1e79fe610d98498afeab15d68cbe064>
2022-11-12 22:45:42 +0100

    added convert endpoint and formatted the code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from schemas import GetAllCurrenciesResponseSchema
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to Team Bevel Black Market Exchange Rate Service"}
@@ -15,20 +16,86 @@ def get_all_currencies() -> Any:
     """
     This endpoint returns all available and supported currencies.
     """
-    return {'success': True, 'message': 'All available currencies retrived successfully.', 'data': services.get_currency_list() }
+    return {
+        "success": True,
+        "message": "All available currencies retrived successfully.",
+        "data": services.get_currency_list(),
+    }
 
 
 @app.get("/rates")
-async def get_currency_rate(currency_code: Union[str, None] = Query(default='NGN', max_length=3, min_length=3)) -> Any:
+async def get_currency_rate(
+    currency_code: Union[str, None] = Query(default="NGN", max_length=3, min_length=3)
+) -> Any:
     """
-    This endpoint returns the exchange rate of a given currency. It defaults to `NGN` when a 
+    This endpoint returns the exchange rate of a given currency. It defaults to `NGN` when a
     currency_code is not provided. `currency_code`: It's a 3 letter code.
     """
+    currency_code = currency_code.upper()
+    if currency_code not in services.get_currency_list():
+        return {"success": False, "message": "Please send a valid currency_code."}
     try:
         resp_data = await services.get_binancep2p_rate(currency_code)
         formatted_data = await services.format_response_data(resp_data)
-        buy_rate = formatted_data[0]['adv']['price']
-        sell_rate = formatted_data[1]['adv']['price']
-        return {'success': True, 'message':f'Exchange rate for {currency_code} retrieved successfully.', 'base_currency': 'USD', 'currency': currency_code, 'buy': buy_rate, 'sell': sell_rate }
+        return {
+            "success": True,
+            "message": f"Exchange rate for {currency_code} retrieved successfully.",
+            "base_currency": "USD",
+            "currency": currency_code,
+            "buy": formatted_data["buy_rate"],
+            "sell": formatted_data["sell_rate"],
+        }
     except:
-        return {'success': False, 'message': 'Unable to get rate'}
+        return {"success": False, "message": "Unable to get rate"}
+
+
+@app.get("/convert")
+async def convert_currency(
+    *,
+    from_currency: str = Query(default="NGN", max_length=3, min_length=3),
+    to_currency: str = Query(default="USD", max_length=3, min_length=3),
+    amount: str,
+) -> Any:
+    """
+    This endpoint converts amount from one currency to another.
+    `amount`: The amount you want to covert
+    `from`: The currency you want to convert from.
+    `to`: The currency you want to convert to.
+    """
+    currency_list = services.get_currency_list()
+    if (
+        from_currency.upper() not in currency_list
+        or to_currency.upper() not in currency_list
+    ):
+        return {"success": False, "message": "Please send a valid currency_code."}
+
+    # FORMULAR
+    # Get usd value of both from_currency and to_currency
+    # Divide amount_given by from_usd_rate
+    # multiply the result by to_usd_rate to get actual rate.
+
+    try:
+        from_currency_resp_data = await services.get_binancep2p_rate(from_currency)
+        to_currency_resp_data = await services.get_binancep2p_rate(to_currency)
+        from_currency_formatted_data = await services.format_response_data(
+            from_currency_resp_data
+        )
+        to_currency_formatted_data = await services.format_response_data(
+            to_currency_resp_data
+        )
+
+        from_rate_in_usd = float(from_currency_formatted_data["buy_rate"])
+        to_rate_in_usd = float(to_currency_formatted_data["buy_rate"])
+
+        result = float(amount) / from_rate_in_usd * to_rate_in_usd
+
+        result = "{:,.2f}".format(result)
+
+        return {
+            "amount": amount,
+            "from": from_currency,
+            "to": to_currency,
+            "total": result,
+        }
+    except:
+        return {"success": False, "message": "Unable to get rate"}
--- services.py
+++ services.py
@@ -3,75 +3,76 @@ from typing import Any, Dict, List, Optional
 import aiohttp
 from schemas import BinanceRequestSchema, BinanaceResponseSchema
 
-binancep2p_endpoint = 'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search'
+binancep2p_endpoint = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
 request_headers = {"Cache-Control": "no-cache", "Content-Type": "application/json"}
 
-def get_currency_list()-> List[str]:
+
+def get_currency_list() -> List[str]:
     currency_list = [
-            "ARS",
-            "EUR",
-            "USD",
-            "AED",
-            "AUD",
-            "BDT",
-            "BHD",
-            "BOB",
-            "BRL",
-            "CAD",
-            "CLP",
-            "CNY",
-            "COP",
-            "CRC",
-            "CZK",
-            "DOP",
-            "DZD",
-            "EGP",
-            "GBP",
-            "GEL",
-            "GHS",
-            "HKD",
-            "IDR",
-            "INR",
-            "JPY",
-            "KES",
-            "KHR",
-            "KRW",
-            "KWD",
-            "KZT",
-            "LAK",
-            "LBP",
-            "LKR",
-            "MAD",
-            "MMK",
-            "MXN",
-            "MYR",
-            "NGN",
-            "OMR",
-            "PAB",
-            "PEN",
-            "PHP",
-            "PKR",
-            "PLN",
-            "PYG",
-            "QAR",
-            "RON",
-            "RUB",
-            "SAR",
-            "SDG",
-            "SEK",
-            "SGD",
-            "THB",
-            "TND",
-            "TRY",
-            "TWD",
-            "UAH",
-            "UGX",
-            "UYU",
-            "VES",
-            "VND",
-            "ZAR"
+        "ARS",
+        "EUR",
+        "USD",
+        "AED",
+        "AUD",
+        "BDT",
+        "BHD",
+        "BOB",
+        "BRL",
+        "CAD",
+        "CLP",
+        "CNY",
+        "COP",
+        "CRC",
+        "CZK",
+        "DOP",
+        "DZD",
+        "EGP",
+        "GBP",
+        "GEL",
+        "GHS",
+        "HKD",
+        "IDR",
+        "INR",
+        "JPY",
+        "KES",
+        "KHR",
+        "KRW",
+        "KWD",
+        "KZT",
+        "LAK",
+        "LBP",
+        "LKR",
+        "MAD",
+        "MMK",
+        "MXN",
+        "MYR",
+        "NGN",
+        "OMR",
+        "PAB",
+        "PEN",
+        "PHP",
+        "PKR",
+        "PLN",
+        "PYG",
+        "QAR",
+        "RON",
+        "RUB",
+        "SAR",
+        "SDG",
+        "SEK",
+        "SGD",
+        "THB",
+        "TND",
+        "TRY",
+        "TWD",
+        "UAH",
+        "UGX",
+        "UYU",
+        "VES",
+        "VND",
+        "ZAR",
     ]
-    return currency_list 
+    return currency_list
 
 
 async def make_post_request(
@@ -86,27 +87,36 @@ async def get_binancep2p_rate(currency_code: str) -> Optional[Dict[str, Any]]:
     """
     `currency_code`: 3 letter code
     """
-    d1 = BinanceRequestSchema(fiat=currency_code, tradeType='sell').dict()
-    d2 = BinanceRequestSchema(fiat=currency_code, tradeType='buy').dict()
+    d1 = BinanceRequestSchema(fiat=currency_code, tradeType="sell").dict()
+    d2 = BinanceRequestSchema(fiat=currency_code, tradeType="buy").dict()
     async with aiohttp.ClientSession() as session:
         tasks = []
         for data in [d1, d2]:
-            tasks.append(asyncio.ensure_future(make_post_request(session, binancep2p_endpoint, data)))
+            tasks.append(
+                asyncio.ensure_future(
+                    make_post_request(session, binancep2p_endpoint, data)
+                )
+            )
 
         results = await asyncio.gather(*tasks)
         return results
 
 
 async def format_response_data(response_data: List[Dict[str, Any]]) -> Any:
+    """
+    Takes a Binance p2p endpoint response and extract the rates.
+    """
     formatted_data = []
     for d in response_data:
         formatted_data.append(BinanaceResponseSchema(**d).dict())
-    
-    # Get the BUY Data
-    buy_data = formatted_data[1]['data'][5]
 
-    # Get the SELL Data
-    sell_data = formatted_data[0]['data'][5]
+    # Get the BUY Data and [5] being the average.
+    buy_data = formatted_data[1]["data"][5]
 
-    return [buy_data, sell_data]
-    
+    # Get the SELL Data and [5] being the average.
+    sell_data = formatted_data[0]["data"][5]
+
+    buy_rate = buy_data["adv"]["price"]
+    sell_rate = sell_data["adv"]["price"]
+
+    return {"buy_rate": buy_rate, "sell_rate": sell_rate}
